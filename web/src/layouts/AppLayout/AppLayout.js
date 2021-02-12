577d616570e51ae848b0313bc1d4bb6c418d6f9d
Venkatesh <venkateshfriends123@gmail.com>
2021-02-12 07:45:15 -0600

    remove commented code

--- web/src/layouts/AppLayout/AppLayout.js
+++ web/src/layouts/AppLayout/AppLayout.js
@@ -4,14 +4,7 @@ const AppLayout = ({ children }) => {
   return (
     <div>
       <h1>Streaming Auction Market Place</h1>
-      {/* <nav>
-        <ul>
-          <li>
-            <Link to={routes.home()}>Home</Link>
-            <Link to={routes.about()}>About</Link>
-          </li>
-        </ul>
-      </nav> */}
+
       <main>{children}</main>
     </div>
   )
